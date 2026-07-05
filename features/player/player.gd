0fbdd2b81a2cef4e34cb957a37e8298740a876b4
Timothy Rodriguez <timmy_258@hotmail.com>
2026-07-05 13:30:09 -0400

    Fixed iteration bugs from swap-with-last on player modifiers

--- features/player/player.gd
+++ features/player/player.gd
@@ -84,9 +84,15 @@ func advance_frame(input_mask: int) -> void:
 	# Process tickers
 	skill_manager.process_tickers()
 	
-	for i in range(_player_modifiers.size() - 1, -1, -1):
-		if _player_modifiers[i] and not _player_modifiers[i].tick_and_check():
-			remove_modifier_at(i)
+	var index: int = 0
+	while index < _player_modifiers_count:
+		# Check if modifier's ticker expired
+		if not _player_modifiers[index].tick_and_check():
+			# Decrement index when swap occured during removal
+			if remove_modifier_at(index):
+				index -= 1
+		
+		index += 1
 	
 	is_recovering = fp_recovery_ticks > 0
 	if is_recovering:
@@ -116,9 +122,8 @@ func advance_frame(input_mask: int) -> void:
 		restrict_attack = false
 		restrict_skills = false
 		
-		for mod in _player_modifiers:
-			if mod:
-				mod.apply()
+		for i in range(_player_modifiers_count):
+			_player_modifiers[i].apply()
 		
 		_compute_speed()
 		_player_modifiers_is_dirty = false
@@ -160,21 +165,24 @@ func add_modifier(modifier: PlayerModifier) -> void:
 	
 	_player_modifiers_is_dirty = true
 
-func remove_modifier_at(index: int) -> void:
+func remove_modifier_at(index: int) -> bool:
 	var last_filled_index: int = _player_modifiers_count - 1
+	var swap_required: bool = index != last_filled_index
 	
-	if index == last_filled_index:
-		_player_modifiers[index] = null
-	else:
+	if swap_required:
 		_player_modifiers[index] = _player_modifiers[last_filled_index]
 		_player_modifiers[last_filled_index] = null
+	else:
+		_player_modifiers[index] = null
 		
 	_player_modifiers_count -= 1
 	_player_modifiers_is_dirty = true
+	
+	return swap_required
 
-func remove_modifier(modifier: PlayerModifier) -> void:
+func remove_modifier(modifier: PlayerModifier) -> bool:
 	var index: int = _player_modifiers.find(modifier)
-	remove_modifier_at(index)
+	return remove_modifier_at(index)
 
 # --- Projectile request wrappers ---
 func add_projectile_request(request: ProjectileRequest) -> void:
