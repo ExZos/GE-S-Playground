f76301db9e49a9d5911393a96b6b3e4d2dc491d6
Timothy Rodriguez <timmy_258@hotmail.com>
2026-08-31 17:49:35 -0400

    Switched Wanderer decision-making movement to use angles

--- features/enemies/catalog/wanderer/wanderer.gd
+++ features/enemies/catalog/wanderer/wanderer.gd
@@ -4,12 +4,17 @@ class_name Wanderer
 
 @export var collision_shape: SGCollisionShape2D
 
+const ANGLES_COUNT: int = 8
+
 var fp_speed: int
 
 var fp_min_action_duration: int
 var fp_max_action_duration: int
 
 var action_ticks: int
+
+var fp_rads: Array[int]
+var angle_weights: Array[int]
 
 func init(data: EnemyData) -> void:
 	super(data)
@@ -22,6 +27,16 @@ func init(data: EnemyData) -> void:
 	fp_max_action_duration = data.fp_max_action_duration
 	
 	action_ticks = 0
+	
+	var fp_degrees: int = 0
+	var fp_degrees_inc: int = SGFixed.from_int(360 / ANGLES_COUNT)
+	for i in range(ANGLES_COUNT):
+		fp_rads.append(SGFixed.div(SGFixed.mul(fp_degrees, SGFixed.PI), SGFixed.from_int(180)))
+		print(fp_rads[i])
+		
+		fp_degrees += fp_degrees_inc
+	
+	angle_weights.resize(ANGLES_COUNT)
 
 func advance_frame(rng: RandomNumberGenerator) -> void:
 	if action_ticks > 0:
@@ -29,11 +44,16 @@ func advance_frame(rng: RandomNumberGenerator) -> void:
 	else:
 		action_ticks = rng.randi_range(fp_min_action_duration, fp_max_action_duration)
 		
+		# TODO: investigate bug causing to simulate a collision even after the colliding node moves away
 		# TODO: pick from available directions to prevent repeated collisions
+		# TODO: test angles with test_move
+		# TODO: secondary index array for sorting?
 		# TODO: change direction on collision (maybe except with player)
-		# TODO: consider preventing no movement actions
-		velocity.x = fp_speed * randi_range(-1, 1)
-		velocity.y = fp_speed * randi_range(-1, 1)
+		velocity.x = SGFixed.ONE
+		velocity.y = 0
+		
+		velocity = velocity.rotated(fp_rads[rng.randi_range(0, ANGLES_COUNT - 1)])
+		velocity.imul(fp_speed)
 		
 		print("NEW ACTION: %dms" % SGFixed.to_int(action_ticks))
 	
