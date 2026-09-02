ece8f2a3440abcb6c0d901dbac670b4872b3c64b
Timothy Rodriguez <timmy_258@hotmail.com>
2026-09-02 15:55:21 -0400

    Randomized Wanderer's angle cycling direction

--- features/enemies/catalog/wanderer/wanderer.gd
+++ features/enemies/catalog/wanderer/wanderer.gd
@@ -5,6 +5,8 @@ class_name Wanderer
 @export var collision_shape: SGCollisionShape2D
 
 const ANGLES_COUNT: int = 8
+const INDEX_SHIFTS: PackedInt32Array = [-1, 1]
+const WEIGHT_PENALTY: int = 60
 
 var fp_speed: int
 
@@ -13,9 +15,10 @@ var fp_max_action_duration: int
 
 var action_ticks: int
 var action_rads_index: int
+var action_rads_index_shift: int
 
 var fp_rads: Array[int]
-var angle_weights: Array[int]
+#var rads_weights: Array[int]
 
 func init(data: EnemyData) -> void:
 	super(data)
@@ -36,9 +39,12 @@ func init(data: EnemyData) -> void:
 		
 		fp_deg += fp_deg_inc
 	
-	angle_weights.resize(ANGLES_COUNT)
+	#rads_weights.resize(ANGLES_COUNT)
 
 func advance_frame(rng: RandomNumberGenerator) -> void:
+	#for i in range(rads_weights.size()):
+		#rads_weights[i] = max(rads_weights[i] - 1, 0)
+	
 	velocity.x = SGFixed.ONE
 	velocity.y = 0
 	
@@ -46,8 +52,9 @@ func advance_frame(rng: RandomNumberGenerator) -> void:
 		action_ticks -= SGFixed.ONE
 	else:
 		action_ticks = rng.randi_range(fp_min_action_duration, fp_max_action_duration)
+		action_rads_index_shift = INDEX_SHIFTS[rng.randi_range(0, 1)]
 		
-		action_rads_index = rng.randi_range(0, ANGLES_COUNT - 1)
+		action_rads_index = rng.randi_range(0, fp_rads.size() - 1)
 		
 		print("NEW ACTION: %dms" % SGFixed.to_int(action_ticks))
 	
@@ -56,7 +63,12 @@ func advance_frame(rng: RandomNumberGenerator) -> void:
 	
 	move_and_slide()
 	
-	# TODO: decaying angle weights that track collisions
 	# TODO: secondary index array for sorting
+	# TODO: select movement angle based on sorted index array
+	# TODO: handle ties so that it doesn't always default to the same angle
 	if get_slide_count() > 0:
-		action_ticks = (action_ticks + 1) % ANGLES_COUNT
+		#rads_weights[action_rads_index] += WEIGHT_PENALTY
+		
+		action_rads_index = posmod(action_rads_index + action_rads_index_shift, fp_rads.size())
+		
+		print("COLLISION: ", action_rads_index)
