dea5bd365672a6c952fbbd8e29692d0f115069c1
Timothy Rodriguez <timmy_258@hotmail.com>
2026-09-17 16:15:36 -0400

    Made ChargingSkillSlot's look consistent with the other skill slots

--- features/skills/ui/skill_tray/skill_slots/charging_skill_slot.gd
+++ features/skills/ui/skill_tray/skill_slots/charging_skill_slot.gd
@@ -17,7 +17,7 @@ var last_state: int
 func _ready() -> void:
 	cooldown_progress_bar.min_value = 0
 	cooldown_progress_bar.max_value = skill._fp_cooldown
-	cooldown_progress_bar.visible = false
+	cooldown_progress_bar.value = skill._fp_cooldown
 	
 	charge_progress_bar.min_value = 0
 	charge_progress_bar.max_value = skill._fp_charge_time
@@ -29,24 +29,20 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	# Determine cooldown/charging time
 	if skill.state == ChargingSkill.State.COOLDOWN:
-		cooldown_progress_bar.value = skill.fp_cd_ticks
+		cooldown_progress_bar.value = skill._fp_cooldown - skill.fp_cd_ticks
 		progress_label.text = "%.1fs" % (skill.fp_cd_ticks / fp_fps)
 	elif skill.state == ChargingSkill.State.CHARGING:
 		charge_progress_bar.value = skill.fp_charge_ticks
 		progress_label.text = "%d%%" % (charge_progress_bar.ratio * 100)
 	
-	# Show cooldown/charging progress bar depending on state
 	if last_state != skill.state:
 		if skill.state == ChargingSkill.State.COOLDOWN:
-			charge_progress_bar.visible = false
-			cooldown_progress_bar.visible = true
-		else:
-			charge_progress_bar.visible = true
-			cooldown_progress_bar.visible = false
-			
-			if skill.state == ChargingSkill.State.IDLE:
-				charge_progress_bar.value = 0
-				cooldown_progress_bar.value = skill._fp_cooldown
-				progress_label.text = ""
-				
+			cooldown_progress_bar.tint_progress = Color.GRAY
+			charge_progress_bar.value = 0
+		elif skill.state == ChargingSkill.State.IDLE:
+			cooldown_progress_bar.value = skill._fp_cooldown
+			cooldown_progress_bar.tint_progress = Color.WHITE
+			charge_progress_bar.value = 0
+			progress_label.text = ""
+		
 		last_state = skill.state
